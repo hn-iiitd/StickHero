0e0fa390fafa9059e0fb207eb7fe48cb99a7db1d
Harsh Nangia <harsh22199@iiitd.ac.in>
2023-11-07 11:46:08 +0530

    updated

--- src/main/java/com/iiitd/stickhero/HelloApplication.java
+++ src/main/java/com/iiitd/stickhero/HelloApplication.java
@@ -10,11 +10,14 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
+
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
         stage.setTitle("StickHero");
         Image icon = new Image("unnamed.png");
+
         stage.getIcons().add(icon);
         stage.setHeight(520);
         stage.setWidth(460);
@@ -22,6 +25,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
 
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/iiitd/stickhero/HelloController.java
+++ src/main/java/com/iiitd/stickhero/HelloController.java
@@ -1,14 +1,27 @@
 package com.iiitd.stickhero;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    public void up(ActionEvent e){
+        System.out.println("UP");
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
     }
+    @FXML
+    public void down(ActionEvent e){
+        System.out.println("DOWN");
+
+    }
+    @FXML
+    public void left(ActionEvent e){
+        System.out.println("LEFT");
+    }
+    @FXML
+    public void right(ActionEvent e){
+        System.out.println("RIGHT");
+    }
+
 }
