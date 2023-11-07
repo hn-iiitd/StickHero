a00d0623c6fbfe6302ab85f8a6a751fd3731bf96
Harsh Nangia <harsh22199@iiitd.ac.in>
2023-11-07 11:20:13 +0530

    updated

--- src/main/java/com/iiitd/stickhero/HelloApplication.java
+++ src/main/java/com/iiitd/stickhero/HelloApplication.java
@@ -3,6 +3,7 @@ package com.iiitd.stickhero;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +13,14 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("StickHero");
+        Image icon = new Image("unnamed.png");
+        stage.getIcons().add(icon);
+        stage.setHeight(520);
+        stage.setWidth(460);
+        stage.setResizable(false);
         stage.setScene(scene);
+
         stage.show();
     }
 
