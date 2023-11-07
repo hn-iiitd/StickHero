d129bafa8c9a0dc2c5c2503e252803d31d6ec480
Harsh Nangia <harsh22199@iiitd.ac.in>
2023-11-07 12:38:43 +0530

    updated

--- src/main/java/com/iiitd/stickhero/Player.java
+++ src/main/java/com/iiitd/stickhero/Player.java
@@ -1,5 +1,25 @@
 package com.iiitd.stickhero;
 
 public class Player {
-    private int height ;
+
+    private int player_currentScore;
+    private int player_highestScore = 0;
+
+    public int getPlayer_currentScore() {
+        return player_currentScore;
+    }
+
+    public void setPlayer_currentScore(int player_currentScore) {
+        this.player_currentScore = player_currentScore;
+    }
+
+    public int getPlayer_highestScore() {
+        return player_highestScore;
+    }
+
+    public void setPlayer_highestScore(int player_highestScore) {
+        this.player_highestScore = player_highestScore;
+    }
+
+
 }
