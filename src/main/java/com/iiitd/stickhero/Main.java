38c8dcf44f960cc4bff5c5563ecaeb2959c12ab5
Harsh Nangia <harsh22199@iiitd.ac.in>
2023-11-07 21:10:40 +0530

    updated

--- src/main/java/com/iiitd/stickhero/Main.java
+++ src/main/java/com/iiitd/stickhero/Main.java
@@ -14,8 +14,13 @@ public class Main extends Application {
     @Override
 
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        //----to_add_css-----
+//        String css = this.getClass().getResource("Style.css").toExternalForm();
+//        scene.getStylesheets().add(css);
+        //----------------------------
+
 
         stage.setTitle("StickHero");
         Image icon = new Image("unnamed.png");
@@ -23,7 +28,7 @@ public class Main extends Application {
         stage.getIcons().add(icon);
         stage.setHeight(520);
         stage.setWidth(460);
-        stage.setResizable(false);
+//        stage.setResizable(false);
         stage.setScene(scene);
 
         stage.show();
--- src/main/java/com/iiitd/stickhero/MainController.java
+++ src/main/java/com/iiitd/stickhero/MainController.java
@@ -2,25 +2,42 @@ package com.iiitd.stickhero;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
+
+    @FXML
+    private Circle myCircle;
+    private double x;
+    private double y;
     @FXML
     public void up(ActionEvent e){
-        System.out.println("UP");
-
+        myCircle.setCenterY(y-=10);
     }
     @FXML
     public void down(ActionEvent e){
-        System.out.println("DOWN");
-
+        myCircle.setCenterY(y+=10);
     }
     @FXML
     public void left(ActionEvent e){
-        System.out.println("LEFT");
+        myCircle.setCenterX(x-=10);
     }
     @FXML
     public void right(ActionEvent e){
-        System.out.println("RIGHT");
+        myCircle.setCenterX(x+=10);
     }
-
+    public void switchToScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Main-view2.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
