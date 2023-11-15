c8defe7139f100fb1abc8cefa67f6d834f6a9039
Guneet Pal Singh <guneet22190@iiitd.ac.in>
2023-11-15 12:21:18 +0530

    added some gui files

--- src/main/java/com/iiitd/stickhero/GamePlayController.java
+++ src/main/java/com/iiitd/stickhero/GamePlayController.java
@@ -1,18 +1,25 @@
 package com.iiitd.stickhero;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class GamePlayController {
+    @FXML
     private Stage stage;
+    @FXML
     private Scene scene;
     private Parent root;
+    @FXML
+    private Rectangle stick;
 
     public void switchToMenu(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Mainmenu.fxml"));
@@ -21,5 +28,13 @@ public class GamePlayController {
         stage.setScene(scene);
         stage.show();
     }
+    public void increase_length(){
+        scene.setOnKeyPressed(e->{
+            if (e.getCode()== KeyCode.SPACE){
+                stick.setHeight(stick.getHeight()+10);
+            }
+        });
+    }
 
+//    scene.setOnKeyPressed(e->)
 }
