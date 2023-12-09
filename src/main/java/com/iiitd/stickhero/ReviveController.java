7dea13aacfa22cb0976d7dbba75345d0ea459f2c
Guneet Pal Singh <guneet22190@iiitd.ac.in>
2023-12-09 17:58:11 +0530

    update

--- src/main/java/com/iiitd/stickhero/ReviveController.java
+++ src/main/java/com/iiitd/stickhero/ReviveController.java
@@ -1,10 +1,12 @@
 package com.iiitd.stickhero;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +18,8 @@ public class ReviveController {
     private int RedCherryCount;
     private int BlueCherryCount;
     private int score;
+    @FXML
+    private Label error;
 
     public void set_data(int red,int blue,int score){
         this.BlueCherryCount=blue;
@@ -29,19 +33,27 @@ public class ReviveController {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("NewGame.fxml"));
             Parent root = loader.load();
             GamePlayController revive = loader.getController();
-            revive.setdata(RedCherryCount, BlueCherryCount, score);
+            revive.setdata(RedCherryCount, BlueCherryCount-3, score);
             Scene scene = new Scene(root);
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(scene);
             stage.show();
         }
+        else{
+            error.setText("NOT ENOUGH BLUE CHERRIES");
+
+
+        }
 
     }
 
-    public void switchToCredits(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Credit.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+    public void switchToGameOver(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("game_over.fxml"));
+        Parent root = loader.load();
+        GameOverController revive = loader.getController();
+        revive.set_data(RedCherryCount, BlueCherryCount, score);
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
